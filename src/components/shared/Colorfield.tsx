ef8ebd39092de5726650053023b74cda5f3b57a9
Ian (TSG) <127363212+jhtsg@users.noreply.github.com>
2025-05-03 10:01:44 -0400

    Add color picker for color box

--- src/components/shared/Colorfield.tsx
+++ src/components/shared/Colorfield.tsx
@@ -1,14 +1,36 @@
-import { InputAdornment, TextField } from "@mui/material";
+import { ClickAwayListener, IconButton, InputAdornment, Popper, TextField, Tooltip } from "@mui/material";
+import { useState } from "react";
+import { SketchPicker } from 'react-color';
 
-export default function Colorfield(props:{
-    color:string;
-    setColor: (color:string) => void;
-    label:string
-}){
+export default function Colorfield(props: {
+    color: string;
+    setColor: (color: string) => void;
+    label: string
+}) {
 
-    const {color,setColor,label} = props;
-    return <TextField label={label} value={color} onChange={(e) => setColor(e.target.value)} slotProps={{input:{startAdornment:<InputAdornment position="start">
-        <div style={{width:'20px', height:'20px', backgroundColor:color, borderRadius:'2px'}}></div>
-    </InputAdornment>}}}/>
+    const { color, setColor, label } = props;
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+    return <>
+        <TextField
+            label={label} value={color}
+            onChange={(e) => setColor(e.target.value)} slotProps={{
+                input: {
+                    startAdornment:
+                        <InputAdornment position="start">
+                            <Tooltip title="Pick a color">
+                                <IconButton onClick={(e) => { setAnchorEl(e.currentTarget) }}>
+                                    <div style={{ width: '20px', height: '20px', backgroundColor: color, borderRadius: '1px', border: 'gray 2px dashed' }} />
+                                </IconButton>
+                            </Tooltip>
+                        </InputAdornment>
+                }
+            }} />
+        <Popper open={!!anchorEl} anchorEl={anchorEl} sx={{ zIndex: 1500 }}>
+            <ClickAwayListener onClickAway={() => { setAnchorEl(null) }}>
+                <SketchPicker color={color} onChange={(color) => { setColor(color.hex); }} />
+            </ClickAwayListener>
+        </Popper>
+    </>
 
 }
