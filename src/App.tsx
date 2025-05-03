5d1b05d2492c810dfa4a8af54097333804d3ba8c
Ian (TSG) <127363212+jhtsg@users.noreply.github.com>
2025-05-03 09:27:58 -0400

    Even smaller widths
    waos

--- src/App.tsx
+++ src/App.tsx
@@ -36,21 +36,22 @@ export default function App() {
 
   return (
     <div style={vertical ? {} : { height: "100vh", display: "flex", alignItems: "center", flexDirection: "column", overflowY: 'auto' }}>
-      <div style={{ width: '100%', minHeight:'768px', maxWidth: '1500px', padding: '20px', flex: '1', display: 'flex', flexDirection: 'column', overflowY: 'hidden' }}>
+      <div style={{ width: '100%', minHeight: '768px', maxWidth: '1500px', padding: '20px', flex: '1', display: 'flex', flexDirection: 'column', overflowY: 'hidden' }}>
         <h1 style={{ marginBottom: '0px', fontSize: "1.5em" }}>The Slime ID (SLID) Creator</h1>
         <hr style={{ width: '100%' }} />
-        <div style={vertical ? {display:'flex', flexDirection:'column-reverse'} : { display: 'flex', flex: '1', overflowY: 'hidden' }}>
+        <div style={vertical ? { display: 'flex', flexDirection: 'column-reverse' } : { display: 'flex', flex: '1', overflowY: 'hidden' }}>
           <Card style={{ flex: '1', overflowY: 'hidden' }}>
-            <CardForm card={card} flipped={isFlipped} setCard={setCard} setFlipped={setIsFlipped} onUploadClick={onUploadClick} filename={fileName} profileSrc={imageSrc}/>
+            <CardForm card={card} flipped={isFlipped} setCard={setCard} setFlipped={setIsFlipped} onUploadClick={onUploadClick} filename={fileName} profileSrc={imageSrc} />
           </Card>
           <div style={
-            width < 1100 ? 
-            { transform: 'scale(0.4)', marginLeft:'-300px', marginTop:'-170px', marginBottom:'-170px', maxWidth:'1035px', margin:'0 auto'}
-            : { transform: 'scale(0.95)', maxWidth:'1035px', margin:'0 auto'}
+            width < 500 ? { transform: 'scale(0.3)', marginLeft: '-360px', marginTop: '-220px', marginBottom: '-220px', maxWidth: '1035px', margin: '0 auto' } :
+              width < 1100 ?
+                { transform: 'scale(0.4)', marginLeft: '-300px', marginTop: '-170px', marginBottom: '-170px', maxWidth: '1035px', margin: '0 auto' }
+                : { transform: 'scale(0.95)', maxWidth: '1035px', margin: '0 auto' }
           }>
             <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
-              <CardFront card={card} profileSrc={imageSrc}/>
-              <CardBack card={card}/>
+              <CardFront card={card} profileSrc={imageSrc} />
+              <CardBack card={card} />
             </ReactCardFlip>
           </div>
         </div>
@@ -60,7 +61,7 @@ export default function App() {
           <div style={{ fontSize: ".8em" }}>This is not a real ID! I assume no legal liability for anything you generate and what shenanigans you get up to!</div>
           <div style={{ marginTop: '5px', display: 'flex', alignItems: 'center', justifyContent: 'center', gap: '5px', maxWidth: '300px', margin: '5px auto' }}>
             <img src="https://licensebuttons.net/p/zero/1.0/88x31.png" height={'20px'} />
-           
+
           </div>
         </div>
       </div>
